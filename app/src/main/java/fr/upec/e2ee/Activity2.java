72c2f4bb0a639556073334fe7918932050f1c710
mialy <mialysandy@gmail.com>
2023-02-20 17:47:26 +0100

    feat:interface

--- app/src/main/java/fr/upec/e2ee/Activity2.java
+++ app/src/main/java/fr/upec/e2ee/Activity2.java
@@ -1,4 +1,71 @@
 package fr.upec.e2ee;
 
-public class Activity2 {
+import androidx.appcompat.app.AlertDialog;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.DialogInterface;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+
+public class Activity2 extends AppCompatActivity {
+    Button add;
+    AlertDialog dialog;
+    LinearLayout layout;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_2);
+        add=findViewById(R.id.add);
+        layout=findViewById(R.id.container);
+        buildDialog();
+        add.setOnClickListener(new View.OnClickListener(){
+
+            @Override
+            public void onClick(View view) {
+                dialog.show();
+            }
+        });
+    }
+
+    private void buildDialog() {
+        AlertDialog.Builder builder=new AlertDialog.Builder(this);
+        View view=getLayoutInflater().inflate(R.layout.dialog,null);
+
+        EditText name=view.findViewById(R.id.name);
+        builder.setView(view);
+        builder.setTitle("Enter name").setPositiveButton("OK", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialogInterface, int i) {
+                addCard(name.getText().toString());
+            }
+        }).setNegativeButton("Cancel", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialogInterface, int i) {
+
+            }
+        });
+        dialog=builder.create();
+    }
+
+    private void addCard(String name) {
+        View view=getLayoutInflater().inflate(R.layout.card,null);
+
+        TextView nameView= view.findViewById(R.id.name);
+        Button delete= view.findViewById((R.id.name));
+        nameView.setText(name);
+
+        delete.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                layout.removeView(view);
+            }
+        });
+        layout.addView((view));
+
+    }
 }
