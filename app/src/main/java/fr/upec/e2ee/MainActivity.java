d1716474ff31854fead3933f5ca197c2b0c8a15a
mialy <mialysandy@gmail.com>
2023-02-22 10:48:09 +0100

    -bug:app can't run

--- app/src/main/java/fr/upec/e2ee/MainActivity.java
+++ app/src/main/java/fr/upec/e2ee/MainActivity.java
@@ -1,23 +1,26 @@
 package fr.upec.e2ee;
 
+import android.content.Intent;
 import android.os.Bundle;
-import android.view.View;
 import android.view.Menu;
+import android.view.View;
+import android.widget.Button;
 
-import com.google.android.material.snackbar.Snackbar;
-import com.google.android.material.navigation.NavigationView;
-
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.drawerlayout.widget.DrawerLayout;
 import androidx.navigation.NavController;
 import androidx.navigation.Navigation;
 import androidx.navigation.ui.AppBarConfiguration;
 import androidx.navigation.ui.NavigationUI;
-import androidx.drawerlayout.widget.DrawerLayout;
-import androidx.appcompat.app.AppCompatActivity;
+
+import com.google.android.material.navigation.NavigationView;
+import com.google.android.material.snackbar.Snackbar;
 
 import fr.upec.e2ee.databinding.ActivityMainBinding;
 
 public class MainActivity extends AppCompatActivity {
 
+    Button afficher = findViewById(R.id.afficher);
     private AppBarConfiguration mAppBarConfiguration;
     private ActivityMainBinding binding;
 
@@ -41,12 +44,14 @@ public class MainActivity extends AppCompatActivity {
         // Passing each menu ID as a set of Ids because each
         // menu should be considered as top level destinations.
         mAppBarConfiguration = new AppBarConfiguration.Builder(
-                R.id.nav_home, R.id.nav_gallery, R.id.nav_slideshow,R.id.nav_identity)
+                R.id.nav_home, R.id.nav_gallery, R.id.nav_slideshow, R.id.nav_identity)
                 .setOpenableLayout(drawer)
                 .build();
         NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment_content_main);
         NavigationUI.setupActionBarWithNavController(this, navController, mAppBarConfiguration);
         NavigationUI.setupWithNavController(navigationView, navController);
+
+        afficher.setOnClickListener(v -> startActivity(new Intent(getApplicationContext(), Activity2.class)));
     }
 
     @Override
