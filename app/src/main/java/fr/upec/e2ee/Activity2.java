9336f779580d53e7a1a9601be4562f95b4c7c490
mialy <mialysandy@gmail.com>
2023-02-22 23:22:06 +0100

    bug:trying to fix activity2 save

--- app/src/main/java/fr/upec/e2ee/Activity2.java
+++ app/src/main/java/fr/upec/e2ee/Activity2.java
@@ -8,14 +8,18 @@ import android.widget.Button;
 import android.widget.EditText;
 import android.widget.LinearLayout;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
+
+import java.util.ArrayList;
 
 public class Activity2 extends AppCompatActivity {
     Button add;
     AlertDialog dialog;
     LinearLayout layout;
+    int cardCount = 0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -33,7 +37,17 @@ public class Activity2 extends AppCompatActivity {
                 dialog.show();
             }
         });
+
+        if (savedInstanceState != null) {
+            cardCount = savedInstanceState.getInt("cardCount");
+            ArrayList<String> cardContents = savedInstanceState.getStringArrayList("cardContents");
+
+            for (String content : cardContents) {
+                addCard(content);
+            }
+        }
     }
+
 
     private void buildDialog() {
         AlertDialog.Builder builder = new AlertDialog.Builder(this);
@@ -67,9 +81,28 @@ public class Activity2 extends AppCompatActivity {
             @Override
             public void onClick(View v) {
                 layout.removeView(view);
+                Toast.makeText(getBaseContext(), "contacts supprimé", Toast.LENGTH_SHORT).show();
             }
         });
-        layout.addView((view));
 
+        layout.addView((view));
+        cardCount++;
+
+    }
+
+    @Override
+    public void onSaveInstanceState(Bundle savedInstanceState) {
+        super.onSaveInstanceState(savedInstanceState);
+
+        // Stocker les données importantes de l'activité dans l'objet Bundle
+        savedInstanceState.putInt("cardCount", layout.getChildCount());
+
+        ArrayList<String> cardContents = new ArrayList<>();
+        for (int i = 0; i < layout.getChildCount(); i++) {
+            View cardView = layout.getChildAt(i);
+            TextView cardTextView = cardView.findViewById(R.id.name);
+            cardContents.add(cardTextView.getText().toString());
+        }
+        savedInstanceState.putStringArrayList("cardContents", cardContents);
     }
 }
